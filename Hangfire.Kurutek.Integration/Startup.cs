0126d1810d00479d46e2f95462957b1d39b2a060
CihatDaghan <88133939+CihatDaghan@users.noreply.github.com>
2021-08-19 13:17:59 +0300

    Update Startup.cs

--- Hangfire.Kurutek.Integration/Startup.cs
+++ Hangfire.Kurutek.Integration/Startup.cs
@@ -13,13 +13,13 @@ namespace Hangfire.Kurutek.Integration
         public void Configuration(IAppBuilder app)
         {
 
-            //Hangfire kurulumu ve tablolarının hangi veritabanını kullanacağını burada belirtiyoruz. Ben ilk derslerde oluşturduğumuz veritabanını kullanıyorum.
+            
             GlobalConfiguration.Configuration.UseSqlServerStorage(@"Server=DESKTOP-2RFMJGK\SQLEXPRESS;Database=Hangfire.Kurutek.Integration;Integrated Security=true");
 
-            //Hangfire'ın kendi dashboardunu aktif ediyoruz.
+           
             app.UseHangfireDashboard();
 
-            //Hangfire Serverı aktif hale getiriyoruz.
+           
             app.UseHangfireServer();
 
             RecurringJob.AddOrUpdate("N11_Siparis_entegrasyon", () => N11OrdersController.N11Orders(), Cron.MinuteInterval(3));
